95b35d3a93753f8741ffb463f0e88e8cdd7ba326
August R <aggesoft@gmail.com>
2020-09-03 18:30:47 +0200

    reinstall fix

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,11 @@ export default {
 };
 </script>
 
+<style>
+@import "./styles/fonts.min.css"; /* Google Fonts */
+@import "./styles/bulma.min.css"; /* Bulma */
+@import "./styles/all.min.css"; /* FontAwesome */
+</style>
 <style lang="scss">
 @import "./styles/main.scss";
 </style>
